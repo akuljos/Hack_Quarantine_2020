a879b16cd16f1c0f80c49dfbb30804a6749e26c2
Steven Zhu <sz297@cornell.edu>
2020-03-26 17:45:28 -0500

    Updated index.js so individual country data access is possible

--- Node/index.js
+++ Node/index.js
@@ -1,13 +1,19 @@
 const MongoClient = require('mongodb');
+const ObjectId = require('mongodb').ObjectID;
 
 const uri = "mongodb+srv://readwrite:hack4Coronachan@bot-9hvoo.mongodb.net/test?retryWrites=true&w=majority";
 
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-  if(err) throw err;
-  console.log('Connected');
-  const collection = client.db("coronavirus_cases_db").collection("cases_by_country").find({}).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    client.close();
-  });
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true },
+  function(err, client) {
+    if(err) throw err;
+    console.log('Connected');
+
+    const collection = client.db("coronavirus_cases_db").collection("cases_by_country");
+
+    //replace ObjectId with new data
+    const data = collection.findOne({_id: new ObjectId("5e7cf75367b5549bf14745b4")}, function(err, result) {
+      if (err) throw err;
+      console.log(result.Countries[100]);
+      client.close();
+    });
 });
